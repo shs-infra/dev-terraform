50e1cff1ac9c75ff316be06ad2d73915a301675c
shs-infra <sarahosin26+2@gmail.com>
2026-03-31 14:47:39 +0200

    feat: add API Gateway integration, Lambda route, and custom domain support (#18)

--- modules/services/shipment-backend/variables.tf
+++ modules/services/shipment-backend/variables.tf
@@ -8,3 +8,12 @@ variable "stage" {
       error_message = "stage must be one of: dev, stage, prod"
     }
 }
+
+variable "custom_domain" {
+  description = "Custom domain config"
+  type = object({
+    domain_name = string
+    certificate_arn = string
+  })
+  default = null
+}
